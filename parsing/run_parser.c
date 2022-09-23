1c2c38e6d99f83c2ccc5372926a87518cbb584ce
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-23 15:07:33 +0200

    add: placeholder for parsing function

---
+++ parsing/run_parser.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run_parser.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/23 15:05:17 by jallerha          #+#    #+#             */
+/*   Updated: 2022/09/23 15:06:41 by jallerha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+#include "linter.h"
+
+
+// TODO : Run parser and return a list of error
+t_chain_lst	*ft_run_parser(char *path)
+{
+	
+}
