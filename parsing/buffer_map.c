5e84942c85905229288ccc5498dbe3437200d2c5
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-24 23:12:36 +0200

    add: buffer map function

---
+++ parsing/buffer_map.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   buffer_map.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/24 22:38:59 by jallerha          #+#    #+#             */
+/*   Updated: 2022/09/24 23:12:06 by jallerha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+// TODO: This file is dirty, refactor it
+
+char	*ft_init(char **output, char **tmp, int *map_size)
+{
+	*map_size = 0;
+	*output = malloc(0);
+	*tmp = NULL;
+	return (*output);
+}
+
+char	*ft_free_locals(char *line, char *tmp, char *output)
+{
+	free(line);
+	free(tmp);
+	free(output);
+	return (NULL);
+}
+
+/**
+ * @brief This function will buffer the entire file to memory
+ * This function returns NULL if there is any kind of error
+ * @param path Path to the file to buffer
+ * @return char* Pointer to the buffer
+*/
+char	*ft_buffer_map(char *path)
+{
+	char	*output;
+	char	*tmp;
+	char	*line;
+	int		fd;
+	int		map_size;
+
+	ft_init(&output, &tmp, &map_size);
+	if (!output)
+		return (NULL);
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+	line = get_next_line(fd);
+	while (line)
+	{
+		map_size += ft_strlen(line);
+		tmp = ft_strsjoin(output, line);
+		if (!tmp)
+			return (ft_free_locals(line, tmp, output));
+		ft_free_locals(line, NULL, output);
+		output = tmp;
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (output);
+}
