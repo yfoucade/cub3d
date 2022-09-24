d9677fc6812b8384e7355a1831367c12a0f70f3b
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-25 01:26:55 +0200

    add: color / texture parsing + errors

--- parsing/run_parser.c
+++ parsing/run_parser.c
@@ -6,11 +6,24 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 15:05:17 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/24 23:36:51 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/25 01:20:52 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+
+// TODO : Check parsed colors and apply mask depending of if 'valid' == 1
+// TODO : Check if path exists (by trying to opening them, use O_DIRECTORY)
+
+void	ft_check_colors(t_game *game)
+{
+	if (game->floor_color.valid == 0)
+		game->errors |= ERR_INV_F;
+	if (game->ceiling_color.valid == 0)
+	{
+		game->errors |= ERR_INV_C;
+	}
+}
 
 /**
  * @brief Runs the parser on the given file
@@ -32,5 +45,7 @@ t_game	ft_run_parser(char *path)
 		return (game);
 	}
 	ft_name_sanity(path, &game);
+	ft_parse_textures(&game);
+	ft_check_colors(&game);
 	return (game);
 }
