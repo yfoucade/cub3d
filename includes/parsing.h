991f8305f0aecda3a56f9456e9b8a87b09926f24
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-24 23:24:02 +0200

    add: few parsing errors regarding colors / textures

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 13:57:37 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/24 23:20:18 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/24 23:22:52 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,23 @@
 # include <fcntl.h>
 
 // -- Parsing error bitmask --
-
-# define ERR_OPEN 0b00000000000000000000000000000001
-# define ERR_READ 0b00000000000000000000000000000010
-# define ERR_CONT 0b00000000000000000000000000000100
-# define ERR_NAME 0b00000000000000000000000000001000
-# define ERR_BUFF 0b00000000000000000000000000010000
-
+# define ERR_OPEN	 0b00000000000000000000000000000001
+# define ERR_READ	 0b00000000000000000000000000000010
+# define ERR_CONT	 0b00000000000000000000000000000100
+# define ERR_NAME	 0b00000000000000000000000000001000
+# define ERR_BUFF	 0b00000000000000000000000000010000
+# define ERR_MISS_NO 0b00000000000000000000000000100000
+# define ERR_MISS_SO 0b00000000000000000000000001000000
+# define ERR_MISS_WE 0b00000000000000000000000010000000
+# define ERR_MISS_EA 0b00000000000000000000000100000000
+# define ERR_MISS_F	 0b00000000000000000000001000000000
+# define ERR_MISS_C	 0b00000000000000000000010000000000
+# define ERR_INV_NO  0b00000000000000000000100000000000
+# define ERR_INV_SO  0b00000000000000000001000000000000
+# define ERR_INV_WE  0b00000000000000000010000000000000
+# define ERR_INV_EA  0b00000000000000000100000000000000
+# define ERR_INV_F	 0b00000000000000001000000000000000
+# define ERR_INV_C	 0b00000000000000010000000000000000
 // -- Parsing error bitmask --
 
 typedef struct s_color
