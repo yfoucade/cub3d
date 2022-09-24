01b56556dae5ef66836b029adcfdca23fa384d3b
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-25 01:26:21 +0200

    fix: memory issue (missing 0x00)

--- parsing/data_sanity.c
+++ parsing/data_sanity.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 13:50:43 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/24 23:25:31 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/25 00:21:55 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 // allocate a huge amount of memory for a file that is not a valid map.
 // (such as /dev/(u)random or any other kind)
 
-// in order to collect a safe-sample, we will only read 256 bytes in the file
+// in order to collect a safe-sample, we will only read 255 bytes in the file
 // and check if they are ascii or not.
 
 #include "parsing.h"
 
 /**
- * @brief This function takes a path to the map, reads first 256 bytes and
+ * @brief This function takes a path to the map, reads first 255 bytes and
  * checks if they are ascii or not. Update bitmask accordingly.
  *
  * @param fd file descriptor of the file to check
@@ -34,11 +34,12 @@ int	ft_data_sanity(char *path, t_game *game)
 	int		i;
 	char	buffer[256];
 
+	ft_bzero(buffer, 256);
 	fd = open(path, O_RDONLY);
 	i = 0;
 	if (fd == -1)
 		return (game->errors |= ERR_OPEN);
-	if (read(fd, buffer, 256) == -1)
+	if (read(fd, buffer, 255) == -1)
 		return (game->errors |= ERR_READ);
 	close(fd);
 	while (buffer[i])
