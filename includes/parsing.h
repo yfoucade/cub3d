fde587a4d0716a496964f2365b08d75017126968
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-24 23:13:39 +0200

    add: bitmask & new functions

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,16 +6,29 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 13:57:37 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/23 14:54:25 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/24 22:43:24 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-#include "neo_libft.h"
+# include "neo_libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
-int	ft_data_sanity(int fd, char **output);
-int	ft_name_sanity(char *filename);
+// -- Parsing error bitmask --
+# define ERR_OPEN 0b00000000000000000000000000000001
+# define ERR_READ 0b00000000000000000000000000000010
+# define ERR_CONT 0b00000000000000000000000000000100
+# define ERR_NAME 0b00000000000000000000000000001000
+
+// -- Parsing error bitmask --
+
+int				ft_data_sanity(char *path, unsigned long *error_mask);
+int				ft_name_sanity(char *filename, unsigned long *errors);
+unsigned long	ft_run_parser(char *path);
+char			*ft_buffer_map(char *path);
 
 #endif
