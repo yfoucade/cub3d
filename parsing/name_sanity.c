c137e32e6b49569cd481240560309b2fbff1ac95
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-24 23:13:29 +0200

    add: mask name error

--- parsing/name_sanity.c
+++ parsing/name_sanity.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 14:52:54 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/23 15:02:18 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/24 23:12:11 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,19 @@
 
 #include "parsing.h"
 
-int	ft_name_sanity(char *filename)
+/**
+ * @brief Checks if the file name is valid (ends in .cub)
+ * 
+ * @param filename Name of the file to check
+ * @param errors Pointer to the error bitmask
+ * @return int 1 if the name is valid, 0 otherwise
+ */
+int	ft_name_sanity(char *filename, unsigned long *errors)
 {
-	return (ft_endswith(filename, ".cub"));
+	if (!ft_endswith(filename, ".cub"))
+	{
+		*errors |= ERR_NAME;
+		return (0);
+	}
+	return (1);
 }
