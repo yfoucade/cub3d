b286737169602b9099a8391c8acbd58801bd1063
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-24 23:13:14 +0200

    add: basic parsing routine

--- parsing/run_parser.c
+++ parsing/run_parser.c
@@ -6,15 +6,25 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 15:05:17 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/24 22:13:47 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/24 23:11:59 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-
-// TODO : Run parser and return a list of error
-t_chain_lst	*ft_run_parser(char *path)
+/**
+ * @brief Runs the parser on the given file
+ * 
+ * @param path Path to the map to parse
+ * @return unsigned long A bitmask of errors
+ */
+unsigned long	ft_run_parser(char *path)
 {
-	
+	unsigned long	errors;
+
+	errors = 0;
+	if (ft_data_sanity(path, &errors) != 1)
+		return (errors);
+	ft_name_sanity(path, &errors);
+	return (errors);
 }
