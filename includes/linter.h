12940ba4cd73f82ca53b8f11af889d4bf499f39a
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-25 23:29:43 +0200

    init: linter header

---
+++ includes/linter.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   linter.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/25 22:29:26 by jallerha          #+#    #+#             */
+/*   Updated: 2022/09/25 23:00:38 by jallerha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LINTER_H
+# define LINTER_H
+
+# include "parsing.h"
+# include "colors.h"
+# include "printf.h"
+
+void	ft_io_errors(char *filename, unsigned long error_mask);
+void	ft_print_errors(t_game *game);
+void	ft_missing_attributes_error(t_game *game);
+
+#endif
