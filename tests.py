27915a1bcfaa5c5295332c8776d6aa19324b8316
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-25 23:30:12 +0200

    add: automatic unit-testing

---
+++ tests.py
@@ -0,0 +1,22 @@
+import subprocess
+import argparse
+from glob import glob
+
+args = argparse.ArgumentParser()
+args.add_argument('-q', '--quiet', action='store_true', help='Suppress succeeded tests')
+args = args.parse_args()
+
+maps = glob("maps/*")
+
+for map in maps:
+	expected = map.split('/')[-1].split("_")[0] == "ko"
+	got = 0
+	try:
+		subprocess.check_call(["./parsing_check", map], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		got = 0
+	except:
+		got = 1
+	if expected != got:
+		print("Test failed", map, "❌")
+	if expected == got and not args.quiet:
+		print("Tested successfully", map, "✅")
