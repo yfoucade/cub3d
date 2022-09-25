ea6b885c3eba7301665d2ae2c775b81038218ae0
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-25 23:30:04 +0200

    norm: added missing indent

--- includes/debug.h
+++ includes/debug.h
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 00:03:16 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/25 01:08:24 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/25 23:22:06 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 # include "neo_libft.h"
 # include "game.h"
 
-#ifndef DEBUG
-# define DEBUG 1
-#endif
+# ifndef DEBUG
+#  define DEBUG 1
+# endif
 
 void	ft_print_game_struct(t_game *game);
 void	ft_print_color_struct(t_color *game);
