b14653e3db4defe0700a87c9e899c400fe1bd236
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-26 22:12:45 +0200

    fix: do not free map as it is an offsetted pointer

--- memory/destroy_game.c
+++ memory/destroy_game.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 00:09:48 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/25 01:12:44 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/26 22:11:31 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ void	ft_destroy_game(t_game *game)
 	free(game->so_path);
 	free(game->we_path);
 	free(game->no_path);
-	free(game->map_buffer);
 	free(game->file_buffer);
 }
