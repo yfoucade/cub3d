88902c910f4fc3ff9d9eeb87743e208e55091893
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-28 14:53:28 +0200

    add: free lines and map_buffer

--- memory/destroy_game.c
+++ memory/destroy_game.c
@@ -6,12 +6,27 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 00:09:48 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/26 22:11:31 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/28 14:51:50 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "game.h"
+#include "lists.h"
+
+void	ft_destroy_lines(t_chain_lst *map_lines)
+{
+	t_chain_lst	*tmp;
+
+	tmp = map_lines;
+	while (tmp)
+	{
+		free(tmp->content);
+		tmp = tmp->next;
+	}
+	ft_chain_clear(&map_lines);
+}
 
 void	ft_destroy_game(t_game *game)
 {
@@ -20,4 +35,7 @@ void	ft_destroy_game(t_game *game)
 	free(game->we_path);
 	free(game->no_path);
 	free(game->file_buffer);
+	free(game->map_buffer);
+	if (game->map_lines)
+		ft_destroy_lines(game->map_lines);
 }
