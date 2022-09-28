248d0f0ec0384152dd8f16e31094c7a8f1cd25b8
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-28 16:10:42 +0200

    clean: removed unused function

--- parsing/load/buffer_file.c
+++ parsing/load/buffer_file.c
@@ -6,31 +6,12 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 22:38:59 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/28 14:10:46 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/28 16:07:53 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 #include "file.h"
-
-// TODO: This file is dirty, refactor it
-
-char	*ft_init(char **output, char **tmp, int *map_size)
-{
-	*map_size = 0;
-	*output = malloc(1);
-	*output[0] = '\0';
-	*tmp = NULL;
-	return (*output);
-}
-
-char	*ft_free_locals(char *line, char *tmp, char *output)
-{
-	free(line);
-	free(tmp);
-	free(output);
-	return (NULL);
-}
 
 /**
  * @brief This function will buffer the entire file to memory
