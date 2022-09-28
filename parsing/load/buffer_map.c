69be92cfedfb2fba961d338b0817d073df6531b3
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-28 15:36:04 +0200

    final fix: buffering map

--- parsing/load/buffer_map.c
+++ parsing/load/buffer_map.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 15:08:36 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/28 14:51:02 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/28 15:35:44 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,59 @@
 #include "lists.h"
 #include <stdio.h>
 
+void	ft_destroy_old_list(t_chain_lst *old_list)
+{
+	t_chain_lst	*tmp;
+
+	tmp = old_list;
+	while (tmp)
+	{
+		free(tmp->content);
+		tmp = tmp->next;
+	}
+	ft_chain_clear(&old_list);
+}
+
+void	ft_process_lines(t_game *game, int offset, t_chain_lst *old_list)
+{
+	t_chain_lst	*tmp;
+
+	game->map_buffer = ft_strdup(game->file_buffer + offset);
+	ft_destroy_old_list(old_list);
+	game->map_lines = ft_split(game->map_buffer, "\n");
+	tmp = game->map_lines;
+	while (tmp)
+	{
+		ft_rstrip((char *) tmp->content);
+		tmp = tmp->next;
+	}
+}
+
 void	ft_buffer_map(t_game *game)
 {
 	t_chain_lst	*lst;
 	t_chain_lst	*tmp;
 	int			offset;
 	int			parameter_line;
+	int			in_map;
 
 	offset = 0;
+	in_map = 0;
 	parameter_line = 6;
 	lst = ft_split(game->file_buffer, "\n");
 	tmp = lst;
 	while (tmp)
 	{
-		if (is_empty((char *) tmp->content))
+		if (is_empty((char *) tmp->content) && !in_map)
 			offset += ft_strlen((char *) tmp->content) + 1;
 		else if (parameter_line > 0)
 		{
 			offset += ft_strlen((char *) tmp->content) + 1;
 			parameter_line--;
 		}
-		free(tmp->content);
+		else
+			in_map = 1;
 		tmp = tmp->next;
 	}
-	offset--;
-	game->map_buffer = ft_strdup(game->file_buffer + offset);
-	ft_chain_clear(&lst);
-	game->map_lines = ft_split(game->map_buffer, "\n");
+	ft_process_lines(game, offset, lst);
 }
