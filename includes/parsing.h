975f6bce75f16aa62bc42cb461ecaf0a5f48f0ca
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-28 16:12:21 +0200

    add: missing map & buffer fail errors

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 13:57:37 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/28 15:16:51 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/28 16:04:00 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,8 @@
 # define ERR_MUL_SPW 0b00000000000010000000000000000000
 # define ERR_NO_SPW  0b00000000000100000000000000000000
 # define ERR_MAP_HOL 0b00000000001000000000000000000000
+# define ERR_MAP_BUF 0b00000000010000000000000000000000
+# define ERR_MAP_MIS 0b00000000100000000000000000000000
 // -- Map sanity error bitmask --
 
 int		ft_data_sanity(char *path, t_game *game);
--- linter/map_errors.c
+++ linter/map_errors.c
@@ -6,7 +6,7 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 14:32:20 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/28 15:25:01 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/28 16:11:57 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,11 @@ void	ft_map_errors(t_game *game)
 			RESET, game->filename, RESET);
 	if (game->errors & ERR_MAP_HOL)
 		ft_fprintf(STDERR, RED3"fatal: %s'%s' Map is sliced%s\n",
+			RESET, game->filename, RESET);
+	if (game->errors & ERR_MAP_BUF)
+		ft_fprintf(STDERR, RED3"fatal: %s'%s' Failed to buffer map%s\n",
+			RESET, game->filename, RESET);
+	if (game->errors & ERR_MAP_MIS)
+		ft_fprintf(STDERR, RED3"fatal: %s'%s' Map is missing%s\n",
 			RESET, game->filename, RESET);
 }
