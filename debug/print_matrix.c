99020578229b8dfdb464a68e84ab1e29fe810437
Jonas ALLERHAND <jallerha@student.42.fr>
2022-09-29 14:47:12 +0200

    add: print colored matrix

--- debug/print_matrix.c
+++ debug/print_matrix.c
@@ -6,12 +6,33 @@
 /*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 13:55:42 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/29 14:10:36 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/09/29 14:46:28 by jallerha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
+#include "colors.h"
 #include <stdio.h>
+
+void	ft_print_colored(char c)
+{
+	if (c == EMPTY)
+		printf("%s%d%s", GREY46, c, RESET);
+	else if (c == WALL)
+		printf("%s%d%s", MAGENTA2, c, RESET);
+	else if (c == VOID)
+		printf("%s%d%s", MAROON, c, RESET);
+	else if (c == PLAYER_NORTH)
+		printf("%s%d%s", LIGHTGREEN1, c, RESET);
+	else if (c == PLAYER_SOUTH)
+		printf("%s%d%s", LIGHTGREEN1, c, RESET);
+	else if (c == PLAYER_EAST)
+		printf("%s%d%s", LIGHTGREEN1, c, RESET);
+	else if (c == PLAYER_WEST)
+		printf("%s%d%s", LIGHTGREEN1, c, RESET);
+	else if (c < 0)
+		printf("%s?%s\n", RED2, RESET);
+}
 
 void	ft_print_matrix(t_matrix *matrix)
 {
@@ -28,7 +49,7 @@ void	ft_print_matrix(t_matrix *matrix)
 	{
 		while (y < rows)
 		{
-			printf("%d ", ft_get_matrix(matrix, y, x));
+			ft_print_colored(ft_get_matrix(matrix, y, x));
 			y++;
 		}
 		printf("\n");
