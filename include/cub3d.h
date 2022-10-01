51719e65ecfabb486f1909b839665f5185ea1328
yfoucade <yohan.foucade@gmail.com>
2022-10-01 23:56:20 +0200

    build: add includes, defines, and error_msg() prototype

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,21 +6,26 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 16:49:36 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/10/01 23:08:04 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/10/01 23:39:30 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <math.h>
+# include <unistd.h>
 # include "math_lib.h"
+# include "libft.h"
 
 # define FALSE 0
 # define TRUE 1
 
+# define SUCCESS 0
+# define FAILURE 1
 
+int	error_msg(char *msg);
 
 #endif
