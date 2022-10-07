63be277b9678a3f0da4af5d55a9d20e1f145c259
yfoucade <yohan.foucade@gmail.com>
2022-10-07 12:18:13 +0200

    include math.h

--- includes/math_lib.h
+++ includes/math_lib.h
@@ -6,13 +6,14 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 23:01:04 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/10/01 23:10:54 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/10/07 12:17:48 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATH_LIB_H
 # define MATH_LIB_H
 
+# include <math.h>
 # include <stdlib.h>
 
 # define FALSE 0
@@ -32,7 +33,9 @@ t_point	*add(t_point *a, t_point *b, char free_byte);
 t_point	*copy(t_point *a);
 t_point	*create(double x, double y);
 double	dist(t_point *a, t_point *b, char free_byte);
+// double	inner_product(t_point *a, t_point *b, char free_byte);
 t_point	*neg(t_point *a, char free_byte);
+// t_point	*normalize(t_point *a, char free_byte);
 double	norm(t_point *a, char free_byte);
 t_point	*rot(t_point *a, double theta, char free_byte);
 t_point	*scalar_mul(t_point *a, double val, char free_byte);
