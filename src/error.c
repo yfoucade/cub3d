63eb0c645452b3ab2ce56c22aff54cf2c3b2135e
yfoucade <yohan.foucade@gmail.com>
2022-10-07 12:19:24 +0200

    feat: error prints error and newline, and can exit or return a value

--- src/error.c
+++ src/error.c
@@ -6,14 +6,17 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 23:23:35 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/10/03 10:14:54 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/10/03 22:39:23 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	error_msg(char *msg)
+int	error_msg(char *msg, int exit_value)
 {
+	ft_putstr_fd("Error\n", STDERR_FILENO);
 	ft_putstr_fd(msg, STDERR_FILENO);
-	return (FAILURE);
+	if (exit_value >= 0)
+		exit(exit_value);
+	return (-exit_value);
 }
