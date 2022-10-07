cd6cdc035733f2e0d804ffefe1a06765ae475379
yfoucade <yohan.foucade@gmail.com>
2022-10-07 23:24:26 +0200

    feat: add update_dir() and update_plane() functions

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 14:48:10 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/10/07 18:34:08 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/10/07 23:14:08 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,9 @@ int	ft_key_press_hook(int keycode, t_game *game)
 int	ft_key_release_hook(int keycode, t_game *game)
 {
 	ft_switch_key_state(keycode, game, KEY_OFF);
+	printf("new pos: %f, %f\n", game->player_pos.x, game->player_pos.y);
+	print_point("dir = ", game->player_dir, "\n");
+	print_point("plane = ", game->camera_plane, "\n");
 	return (0);
 }
 
@@ -59,21 +62,53 @@ char	key_pressed(t_game *game)
 	return (FALSE);
 }
 
+char	is_in_wall(t_game *game, t_point pos)
+{
+	if (game->map[(int)pos.y][(int)pos.x] == '1')
+		return (TRUE);
+	return (FALSE);
+}
+
 void	update_pos(t_game *game)
 {
-	(void)game;
+	t_point	mov;
+
+	mov = create(0, 0);
+	if (game->pressed_keys.w)
+		mov = add(mov, game->player_dir);
+	if (game->pressed_keys.s)
+		mov = sub(mov, game->player_dir);
+	if (game->pressed_keys.a)
+		mov = sub(mov, get_perp(game->player_dir, GST_DIR_SIZE));
+	if (game->pressed_keys.d)
+		mov = add(mov, get_perp(game->player_dir, GST_DIR_SIZE));
+	mov = normalize(mov);
+	mov = scalar_mul(mov, GST_MOVE_SPEED);
+	mov = add(game->player_pos, mov);
+	if (!is_in_wall(game, mov))
+		game->player_pos = mov;
+}
+
+void	update_dir(t_game *game)
+{
+	if (game->pressed_keys.left && !game->pressed_keys.right)
+		game->player_dir = rot(game->player_dir, GST_ROT_SPEED);
+	if (game->pressed_keys.right && !game->pressed_keys.left)
+		game->player_dir = rot(game->player_dir, -GST_ROT_SPEED);
 }
 
 void	update_plane(t_game *game)
 {
 	// to compute plane, multiply dir by a rotation matrix with param (3*pi/2)
 	// then multiply by target_norm / curr_norm to obtain a vector of target_norm.
-	(void)game;
+	game->camera_plane = get_perp(game->player_dir, GST_PLANE_SIZE);
 }
 
 void	update_pos_dir_plane(t_game	*game)
 {
 	update_pos(game);
+	update_dir(game);
+	update_plane(game);
 }
 
 void	draw_frame(t_game *game)
@@ -85,7 +120,7 @@ int	ft_game_loop(t_game *game)
 {
 	if (key_pressed(game) || game->init_frame)
 	{
-		printf("updating frame\n");
+		// printf("updating frame\n");
 		game->init_frame = FALSE;
 		update_pos_dir_plane(game);
 		draw_frame(game);
