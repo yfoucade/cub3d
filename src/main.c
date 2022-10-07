1b9f1399746937271b7f2234174bf0d706436401
yfoucade <yohan.foucade@gmail.com>
2022-10-07 12:20:15 +0200

    style: trivial main() function

--- src/main.c
+++ src/main.c
@@ -3,41 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 12:18:41 by jallerha          #+#    #+#             */
-/*   Updated: 2022/09/29 17:41:45 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/10/05 22:20:52 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "parsing.h"
-#include "debug.h"
+#include "cub3d.h"
 
-int	main(int argc, char **argv)
+int	main(int ac, char **av)
 {
 	t_game	game;
-	int		ret;
 
-	DEBUG_PRINT("To suppress debug output, compile with -DDEBUG=0\n")
-	ret = 0;
-	if (argc != 2)
-		return (1);
-	game = ft_run_parser(argv[1]);
-	game.filename = argv[1];
-	ft_print_errors(&game);
-	ft_print_matrix(&game.matrix);
-	if (game.errors != 0)
-	{
-		DEBUG_PRINT("non-zero error mask : %llu\n", game.errors);
-		ret = 1;
-	}
-	else
-	{
-		ft_start_game(&game);
-		ret = 0;
-	}
-	ft_destroy_game(&game);
-	return (ret);
+	parse(ac, av, &game);
+	run_game(&game);
+	exit(0);
 }
