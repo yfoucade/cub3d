a90a37d651b8ea983712fe14194f1238fa894040
yfoucade <yohan.foucade@gmail.com>
2022-11-06 22:33:27 +0100

    refactor: compute perpwalldist in rc_run_dda()

--- src/cub3d.c
+++ src/cub3d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 14:48:10 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/10/30 23:39:44 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/11/06 22:31:13 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	draw_walls(t_game *game)
 {
-	int		drawStart;
-	int		drawEnd;
-	int		lineHeight;
+	int		draw_start;
+	int		draw_end;
+	int		line_height;
 	t_img	texture;
-	double	wallX;
+	double	wall_x;
 
 	game->rt_vars.screen_x = -1;
 	while (++game->rt_vars.screen_x < WIN_WIDTH)
@@ -26,23 +26,21 @@ void	draw_walls(t_game *game)
 		rc_set_vars(game);
 		rc_distance(game);
 		rc_run_dda(game);
-		if(game->rt_vars.side == 0)
-			game->rt_vars.perpWallDist = (game->rt_vars.sideDist.x - game->rt_vars.deltaDist.x);
-		else
-			game->rt_vars.perpWallDist = (game->rt_vars.sideDist.y - game->rt_vars.deltaDist.y);
-		rc_calc_bounds(game, &drawStart, &drawEnd, &lineHeight);
+		rc_calc_bounds(game, &draw_start, &draw_end, &line_height);
 		texture = get_texture(game);
-		wallX = rc_wall_collision(game);
-		int texX = (int)(wallX * (double)(texture.width));
-		if(game->rt_vars.side == 0 && game->rt_vars.ray.x < 0) texX = texture.width - texX - 1;
-		if(game->rt_vars.side == 1 && game->rt_vars.ray.y > 0) texX = texture.width - texX - 1;
-      	double step = 1.0 * texture.height / lineHeight;
-      	double texPos = (drawStart - WIN_HEIGHT / 2 + lineHeight / 2) * step;
-		for (int y = drawStart; y < drawEnd; y++)
+		wall_x = rc_wall_collision(game);
+		int	tex_x = (int)(wall_x * (double)(texture.width));
+		if (game->rt_vars.side == 0 && game->rt_vars.ray.x < 0)
+			tex_x = texture.width - tex_x - 1;
+		if (game->rt_vars.side == 1 && game->rt_vars.ray.y > 0)
+			tex_x = texture.width - tex_x - 1;
+		double	step = 1.0 * texture.height / line_height;
+		double	tex_pos = (draw_start - WIN_HEIGHT / 2 + line_height / 2) * step;
+		for (int y = draw_start; y < draw_end; y++)
 		{
-			int texY = (int)texPos & (texture.height - 1);
-			texPos += step;
-			int color = ft_get_pixel(texture, texY, texX);
+			int	tex_y = (int)tex_pos & (texture.height - 1);
+			tex_pos += step;
+			int	color = ft_get_pixel(texture, tex_y, tex_x);
 			ft_put_pixel(game, y, game->rt_vars.screen_x, color);
 		}
 	}
--- src/game/raycasting/dda.c
+++ src/game/raycasting/dda.c
@@ -3,32 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   dda.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 18:30:17 by jallerha          #+#    #+#             */
-/*   Updated: 2022/10/30 18:35:47 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/11/06 22:30:53 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	check_next_square(t_game *game)
+{
+	if (game->rt_vars.sideDist.x < game->rt_vars.sideDist.y)
+	{
+		game->rt_vars.sideDist.x += game->rt_vars.deltaDist.x;
+		game->rt_vars.mapX += game->rt_vars.stepX;
+		game->rt_vars.side = 0;
+	}
+	else
+	{
+		game->rt_vars.sideDist.y += game->rt_vars.deltaDist.y;
+		game->rt_vars.mapY += game->rt_vars.stepY;
+		game->rt_vars.side = 1;
+	}
+}
+
 void	rc_run_dda(t_game *game)
 {
 	while (game->rt_vars.hit == 0)
 	{
-		if (game->rt_vars.sideDist.x < game->rt_vars.sideDist.y)
-		{
-			game->rt_vars.sideDist.x += game->rt_vars.deltaDist.x;
-			game->rt_vars.mapX += game->rt_vars.stepX;
-			game->rt_vars.side = 0;
-		}
-		else
-		{
-			game->rt_vars.sideDist.y += game->rt_vars.deltaDist.y;
-			game->rt_vars.mapY += game->rt_vars.stepY;
-			game->rt_vars.side = 1;
-		}
+		check_next_square(game);
 		if (game->map[game->rt_vars.mapY][game->rt_vars.mapX] == '1')
 			game->rt_vars.hit = 1;
 	}
+	if (game->rt_vars.side == 0)
+		game->rt_vars.perpWallDist
+			= (game->rt_vars.sideDist.x - game->rt_vars.deltaDist.x);
+	else
+		game->rt_vars.perpWallDist
+			= (game->rt_vars.sideDist.y - game->rt_vars.deltaDist.y);
 }
