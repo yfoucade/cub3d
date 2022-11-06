3088fc5bfef26a66ab9aa2ddd42d38380830f67d
yfoucade <yohan.foucade@gmail.com>
2022-11-06 21:07:01 +0100

    norm

--- src/game/raycasting/wall_collision.c
+++ src/game/raycasting/wall_collision.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wall_collision.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 19:35:21 by jallerha          #+#    #+#             */
-/*   Updated: 2022/10/30 19:37:54 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/11/06 21:06:50 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 double	rc_wall_collision(t_game *game)
 {
-	double	wallX;
+	double	wall_x;
 
 	if (game->rt_vars.side == 0)
-		wallX = game->player_pos.y + game->rt_vars.perpWallDist * game->rt_vars.ray.y;
+		wall_x = game->player_pos.y
+			+ game->rt_vars.perpWallDist * game->rt_vars.ray.y;
 	else
-		wallX = game->player_pos.x + game->rt_vars.perpWallDist * game->rt_vars.ray.x;
-	return (wallX - floor(wallX));
+		wall_x = game->player_pos.x
+			+ game->rt_vars.perpWallDist * game->rt_vars.ray.x;
+	return (wall_x - floor(wall_x));
 }
