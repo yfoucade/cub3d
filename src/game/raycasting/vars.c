07aa44ee264ac62cb05be6e6e684eaee17c440b0
yfoucade <yohan.foucade@gmail.com>
2022-11-06 20:24:19 +0100

    norm

--- src/game/raycasting/vars.c
+++ src/game/raycasting/vars.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vars.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jallerha <jallerha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 18:25:07 by jallerha          #+#    #+#             */
-/*   Updated: 2022/10/30 18:28:41 by jallerha         ###   ########.fr       */
+/*   Updated: 2022/11/06 20:24:08 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	rc_set_vars(t_game *game)
 {
 	game->rt_vars.hit = 0;
 	game->rt_vars.camera_x = 2 * game->rt_vars.screen_x / (double)WIN_WIDTH - 1;
-	game->rt_vars.ray = add(game->player_dir, scalar_mul(game->camera_plane, game->rt_vars.camera_x));
+	game->rt_vars.ray = add(
+			game->player_dir,
+			scalar_mul(game->camera_plane, game->rt_vars.camera_x));
 	game->rt_vars.mapX = (int)game->player_pos.x;
 	game->rt_vars.mapY = (int)game->player_pos.y;
 }
